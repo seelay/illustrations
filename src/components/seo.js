5459fedb05a8a2a329f6a656ba3caa9dfe7d923f
Aman Seelay <47111641+ImSeelay@users.noreply.github.com>
2021-12-01 17:39:34 +0530

    ff

--- src/components/seo.js
+++ src/components/seo.js
@@ -45,7 +45,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: `https://illustrations.seelay.in/og.png`,
+          content: `https://illustrations.seelay.in/cover.png`,
         },
         {
           property: `og:title`,
@@ -85,7 +85,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `twitter:image`,
-          content: `https://illustrations.seelay.in/og.png`,
+          content: `https://illustrations.seelay.in/cover.png`,
         },
       ].concat(meta)}
     />
